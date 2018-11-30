01885fb3167aea2673741b6dd7268ab92245900f
tnelles <tim.nelles@denkwerk.com>
2018-11-30 10:15:11 +0100

    Added removed bytes logging

--- tasks/css_clean.js
+++ tasks/css_clean.js
@@ -13,8 +13,11 @@ module.exports = function (grunt) {
     grunt.registerMultiTask('css_clean', 'Remove duplicated css according to application priority', function () {
         var CleanCSS = require('clean-css');
 
+        var files = this.files;
+        var removedBytesSum = 0;
+
         // Iterate over all specified file groups.
-        this.files.forEach(function (file) {
+        files.forEach(function (file, b, c) {
             // Concat specified files.
             file.src.forEach(function (srcpath) {
                 if (!grunt.file.exists(srcpath)) {
@@ -30,8 +33,37 @@ module.exports = function (grunt) {
 
                     grunt.file.write(file.dest, result.styles);
                     grunt.verbose.writeln('File "' + file.dest + '" created.');
+
+                    removedBytesSum += utf8StringSizeBytes(fileContents) - utf8StringSizeBytes(result.styles);
                 }
             });
+
+            var currentIndex = files.findIndex(function(item){
+                return item == file;
+            });
+
+            if (currentIndex === files.length - 1) {
+                var formattedSize = removedBytesSum;
+                var unit = ' B';
+
+                if (formattedSize > 1000) {
+                    formattedSize = formattedSize / 1000;
+                    unit = ' kB';
+                }
+                else if (formattedSize > 1000000) {
+                    formattedSize = formattedSize / 1000000;
+                    unit = ' kB';
+                }
+
+                formattedSize = (parseFloat(formattedSize).toFixed(2) + '').replace('.', ',');
+
+                grunt.log.writeln('Removed ' + formattedSize + unit + '.');
+            }
         });
     });
+
+    function utf8StringSizeBytes(string) {
+        var match = encodeURIComponent(string).match(/%[89ABab]/g);
+        return parseFloat(string.length + (match ? match.length : 0));
+    }
 };
